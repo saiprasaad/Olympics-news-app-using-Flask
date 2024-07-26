a2fe2702379d6e2fdcdfd1b5052d4e033b23bd32
saiprasaad <saiprasaad1999@gmail.com>
2024-07-26 11:45:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 app.register_blueprint(news_blueprint)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, port=8000, host="0.0.0.0")
